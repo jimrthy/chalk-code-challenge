ef042dea8699a60504c68854ced0e3640e85d0da
James Gatannah <jamesgatannah@gmail.com>
2013-10-06 16:09:03 -0500

    Initial code generation, using rails new. Absolutely nothing worth mentioning here

---
+++ gender-guess/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GenderGuess::Application.config.secret_key_base = 'ceee6a0a38ebe0eb9c0d7f271fca93fc11718072507b799510aa7665a0b82a1ce571a6ac7775c29d5bc7cffade229495a4c2a1a578be6b0259f5da3517681c49'

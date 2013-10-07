9d67c930722ae958202662e21823a7013ac19ee2
James Gatannah <jamesgatannah@gmail.com>
2013-10-07 10:26:27 -0500

    Fix routes. Change About page over to HAML

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 GenderGuess::Application.routes.draw do
-  #get "static_pages/home"
+
   root 'static_pages#home'
-  #get "static_pages/help"
-  match '/help', to: 'static_pages/help', via: 'get'
-  #get "static_pages/about"
-  match '/about', to: 'static_pages/about', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

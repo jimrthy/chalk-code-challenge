0f25c6dd5369241966c2e0f242d320646c1b5e43
James Gatannah <jamesgatannah@gmail.com>
2013-10-09 10:52:03 -0500

    Switch to using Zurb Foundation templating.

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ end
 # Use SCSS for stylesheets
 gem 'sass-rails', '4.0.0'
 
+gem 'zurb-foundation', '4.3.2'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '2.1.1'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+# Implements the world-famous Paul Irish IE conditional comments HTML tag--in HAML
+# http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
+# Credit: https://gist.github.com/drewbarontini/2225615
+def cc_html(options={}, &blk)
+  attrs = options.map { |(k, v)| " #{h k}='#{h v}'" }.join('')
+  [ "<!--[if lt IE 7]> <html#{attrs} class='lt-ie9 lt-ie8 lt-ie7'> <![endif]-->",
+    "<!--[if IE 7]>    <html#{attrs} class='lt-ie9 lt-ie8'> <![endif]-->",
+    "<!--[if IE 8]>    <html#{attrs} class='lt-ie9'> <![endif]-->",
+    "<!--[if gt IE 8]><!--> <html#{attrs} class=''> <!--<![endif]-->",
+    capture_haml(&blk).strip,
+    "</html>"
+  ].join("\n")
+end
+def h(str); Rack::Utils.escape_html(str); end
+
 end
